6af1cba6a84e3396b0e8865d61a22daf0dccb530
finkl0119 <finkl0119@gmail.com>
2021-01-05 14:40:39 +0900

    update

---
+++ src/Day7/MultiArray_Example_2.java
@@ -0,0 +1,39 @@
+package Day7;
+
+/**
+ * 
+ * <pre>
+ * Day7
+ * MultiArray_Example_2.java
+ * </pre>
+ *
+ * @Author : '유형민'
+ * @Date   : 2021. 1. 5.	
+ *
+ */
+public class MultiArray_Example_2 {
+
+	public static void main(String[] args) {
+		
+		int intArray[][] = new int[4][];
+		
+		intArray[0] = new int[3];
+		intArray[1] = new int[2];
+		intArray[2] = new int[3];
+		intArray[3] = new int[2];
+		
+		for(int i=0; i<intArray.length; i++) {
+			for(int j=0; j<intArray[i].length; j++) {
+				intArray[i][j] = (i+1) * 10 + j;
+			}
+		}
+		
+		for(int i=0; i<intArray.length; i++) {
+			for(int j=0; j<intArray[i].length; j++) {
+				System.out.print(intArray[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+}
---
+++ src/Day7/MultiArray_Quiz_1.java
@@ -0,0 +1,20 @@
+package Day7;
+
+/**
+ * 
+ * <pre>
+ * Day7
+ * MultiArray_Quiz_1.java
+ * </pre>
+ *
+ * @Author : '유형민'
+ * @Date   : 2021. 1. 5.	
+ *
+ */
+public class MultiArray_Quiz_1 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
