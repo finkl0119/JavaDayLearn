599f730dc5a4d97ac6b50cd943978bdfb625e79a
YU <YU@DESKTOP-T679C96>
2021-02-05 00:23:36 +0900

    UPDATE

---
+++ src/Day21/FileInputOutput_01.java
@@ -0,0 +1,16 @@
+package Day21;
+
+public class FileInputOutput_01 {
+
+	public static void main(String[] args) {
+
+		//========================================
+		
+		System.out.println("===== 42번 문제 =====");	
+		System.out.println();
+
+		
+		
+	}
+
+}
